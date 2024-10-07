9db55209521fed7986bff9bbc74278b0c6a696f8
LeoMEURDRA <leo.meurdra23@gmail.com>
2024-10-07 15:08:59 +0200

    Modification de la classe model Sport pour ajouter un parametre affectant les site à une liste (List<Site> site)

--- src/main/java/bts/sio/webapp/model/Sport.java
+++ src/main/java/bts/sio/webapp/model/Sport.java
@@ -3,9 +3,12 @@ package bts.sio.webapp.model;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Sport {
     private Integer id;
     private String nom;
     private String descriptif;
+    private List<Site> site;
 }
